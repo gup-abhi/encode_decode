fa1476f6e23f8acd7179745c391dea24173f5063
Abhishek Gupta <agabhishek20121998@gmail.com>
2020-04-30 15:49:06 +0530

    initial commit

---
+++ decode.py
@@ -0,0 +1,14 @@
+# specifying text to be encoded
+text = 'Python'
+
+# encoding the text
+encode_text = text.encode()
+
+# decoding the text
+decode_text = encode_text.decode()
+
+# printing the encoded text
+print('Encoded Text : ', encode_text)
+
+# printing the decoed text
+print('Decoded Text : ', decode_text)
